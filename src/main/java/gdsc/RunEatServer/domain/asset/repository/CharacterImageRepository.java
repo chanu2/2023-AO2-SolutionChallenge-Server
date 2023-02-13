7ff7747be0dafa8c50a0320ab5dd3155204da93c
chanu2 <mdsoo55828@gmail.com>
2023-02-13 22:45:43 +0900

    :page_facing_up:: 캐릭터이미지 레포지토리 생성(#2)

---
+++ src/main/java/gdsc/RunEatServer/domain/asset/repository/CharacterImageRepository.java
@@ -0,0 +1,10 @@
+package gdsc.RunEatServer.domain.asset.repository;
+
+import gdsc.RunEatServer.domain.asset.entity.CharacterImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CharacterImageRepository extends JpaRepository<Long, CharacterImage> {
+
+
+
+}
