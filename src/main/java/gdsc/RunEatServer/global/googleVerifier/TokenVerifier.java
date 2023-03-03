bbbce0966f3540c9d8f0ff7d1e4e60be10d32bae
chanu2 <mdsoo55828@gmail.com>
2023-03-03 23:53:40 +0900

    2023.03.03 @Value 추가

--- src/main/java/gdsc/RunEatServer/global/googleVerifier/TokenVerifier.java
+++ src/main/java/gdsc/RunEatServer/global/googleVerifier/TokenVerifier.java
@@ -7,6 +7,7 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
@@ -18,15 +19,19 @@ import java.util.Collections;
 @Configuration
 public class TokenVerifier {
 
+    @Value("${google.clientId}")
+    private String clientId;
+
     private final NetHttpTransport transport = new NetHttpTransport();
     private final JsonFactory jsonFactory = new GsonFactory();
+
+
     public String tokenVerify(String idTokenString) throws GeneralSecurityException, IOException {
 
         log.info("idTokenString={}",idTokenString);
 
-
         GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
-                .setAudience(Collections.singletonList("213617031580-3bjijd93fo1fh4jekjo088s6qbb6p6rt.apps.googleusercontent.com"))
+                .setAudience(Collections.singletonList(clientId))
                 .build();
 
         GoogleIdToken idToken = verifier.verify(idTokenString);
@@ -37,7 +42,6 @@ public class TokenVerifier {
 
             GoogleIdToken.Payload payload = idToken.getPayload();
 
-
             String email = payload.getEmail();
 
             String name = (String) payload.get("name");
@@ -46,10 +50,7 @@ public class TokenVerifier {
             log.info("email={}", email);
             log.info("name={}", name);
             log.info("pictureUrl={}", pictureUrl);
-
-
             return email;
-
         }
         else {
             return null;
